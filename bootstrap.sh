4ad337ef662531703df4ba21b5aeab994dbbc546
Christian Helms <christian@fonticons.com>
2016-12-10 21:02:01 -0600

    adding the routes and styles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,31 @@
+# SQLite version 3.x
+#   gem install sqlite3
+#
+#   Ensure the SQLite 3 gem is defined in your Gemfile
+#   gem 'sqlite3'
+#
+default: &default
+  adapter: postgresql
+  pool: 5
+  host: localhost
+  timeout: 5000
+  username: vagrant
+  password: vagrant
+
+development:
+  <<: *default
+  database: glyphery_dev
+
+# Warning: The database defined as "test" will be erased and
+# re-generated from your development database when you run "rake".
+# Do not set this db to the same as development or production.
+test:
+  <<: *default
+  database: glyphery_test
+
+production:
+  <<: *default
+  database: glyphery
 #!/usr/bin/env bash
 
 apt-get update
@@ -5,4 +33,4 @@ apt-get install -y apache2
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
-fi 
+fi
